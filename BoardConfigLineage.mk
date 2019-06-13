fdd501dbf34da5f2c9a9149c2e565cb01a0dd5f5
Daniel Micay <danielmicay@gmail.com>
2019-06-13 15:23:28 -0400

    set AVB rollback index to security patch timestamp
    This is a backport of the implementation for the Pixel 3.
    
    # Conflicts:
    #	BoardConfig.mk

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -9,7 +9,11 @@ TARGET_KERNEL_ADDITIONAL_FLAGS := \
     MKDTIMG=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/libufdt/mkdtimg
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CLANG_VERSION := 5900059
-TARGET_COMPILE_WITH_MSM_KERNEL := true
+
+# Verified Boot
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2
+
 
 # Audio
 BOARD_SUPPORTS_SOUND_TRIGGER := true
