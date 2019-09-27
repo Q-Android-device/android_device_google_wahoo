d45b4a3683647c61940a158cecf4e2be10d6f5a0
BlackDeath <BlackDeaths@users.noreply.github.com>
2019-09-27 10:44:55 +0200

    Update BoardConfigLineage.mk

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -1,17 +1,16 @@
-# Custom Carbon BoardConfig for wahoo
-# Inline kernel building
-TARGET_KERNEL_CONFIG := wahoo_defconfig
-TARGET_KERNEL_SOURCE := kernel/google/wahoo
+# Common board config for taimen, walleye
+
+# Audio
+BOARD_SUPPORTS_SOUND_TRIGGER := true
+
+# Kernel
 BOARD_KERNEL_IMAGE_NAME := Image.lz4-dtb
 TARGET_COMPILE_WITH_MSM_KERNEL := true
 TARGET_KERNEL_ADDITIONAL_FLAGS := \
     DTC=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/dtc/dtc \
     MKDTIMG=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/libufdt/mkdtimg
 TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_SOURCE := kernel/google/wahoo
 TARGET_NEEDS_DTBOIMAGE := true
-TARGET_COMPILE_WITH_MSM_KERNEL := true
 
-# Audio
-BOARD_SUPPORTS_SOUND_TRIGGER := true
-
-BOARD_PREBUILT_VENDORIMAGE := true
+-include vendor/google/wahoo/BoardConfigVendor.mk
