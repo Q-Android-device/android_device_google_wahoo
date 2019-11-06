2dcc7bcb75fe34a33b33c4ed7f7f8afc7429b7ad
BlackDeaths <blackjubels@gmail.com>
2019-11-06 03:58:22 +0100

    Revert "Revert "Merge commit 'refs/changes/17/1151817/1' of https://android.googlesource.com/device/google/wahoo into 10""
    This reverts commit 5100131a0d9b9a84d558ffcb17ae39d861bec922.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -89,10 +89,10 @@ BOARD_USES_SYSTEM_OTHER_ODEX := true
 
 BOARD_ROOT_EXTRA_FOLDERS := persist firmware metadata
 
-BOARD_SEPOLICY_DIRS += device/google/wahoo/sepolicy/vendor
+BOARD_VENDOR_SEPOLICY_DIRS += device/google/wahoo/sepolicy/vendor
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR := device/google/wahoo/sepolicy/public
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR := device/google/wahoo/sepolicy/private
-BOARD_SEPOLICY_DIRS += device/google/wahoo/sepolicy/verizon
+BOARD_VENDOR_SEPOLICY_DIRS += device/google/wahoo/sepolicy/verizon
 
 TARGET_FS_CONFIG_GEN := device/google/wahoo/config.fs
 
@@ -172,3 +172,12 @@ BOARD_VNDK_VERSION := current
 DEXPREOPT_GENERATE_APEX_IMAGE := true
 
 -include device/google/wahoo/BoardConfigLineage.mk
+
+# Board uses A/B OTA.
+AB_OTA_UPDATER := true
+
+AB_OTA_PARTITIONS += \
+    boot \
+    system \
+    vbmeta \
+    dtbo
